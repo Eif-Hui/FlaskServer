d062028dbfc0d4ca2da496baa4f45eaafe8c6d08
gitadmin <13711153040@163.com>
2020-01-18 17:21:50 +0800

    第一次修改提交

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ tasks = [
     },
     {
         'id': 2,
-        'title': u'Learn Python',
+        'title': 'Learn Python',
         'description': u'Need to find a good Python tutorial on the web',
         'done': False
     }

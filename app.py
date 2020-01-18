38123706728d0d34f0c4a907bf5a91a09d0b164c
gitadmin <13711153040@163.com>
2020-01-18 17:14:02 +0800

    第一次提交app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/18 下午5:06
+# @Author  : Hui
+# @File    : app.py
+
+from flask import Flask,jsonify
+
+
+app = Flask(__name__)
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
